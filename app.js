5bd185b32eeba4c8e241499728bbda4982a8a6ea
Federico Barbieri <federicobarbieri54@gmail.com>
2021-05-14 10:23:41 +0200

    project uploaded

---
+++ app.js
@@ -0,0 +1,182 @@
+
+// ZUCK 1
+
+const zuck1 = document.getElementById('firstzuck');
+
+
+zuck1.addEventListener("click", function () {
+    zuck1.classList.add("visible-zuck1")
+});
+
+// ZUCK 2
+
+const zuck2 = document.getElementById('secondzuck');
+
+
+zuck2.addEventListener("click", function () {
+    zuck2.classList.add("visible-zuck2")
+});
+
+// ZUCK 3
+
+const zuck3 = document.getElementById('thirdzuck');
+
+
+zuck3.addEventListener("click", function () {
+    zuck3.classList.add("visible-zuck3")
+});
+
+
+// ZUCK 4
+
+const zuck4 = document.getElementById('fourthzuck');
+
+
+zuck4.addEventListener("click", function () {
+    zuck4.classList.add("visible-zuck4")
+});
+
+// ZUCK 5
+
+const zuck5 = document.getElementById('fifthzuck');
+
+
+zuck5.addEventListener("click", function () {
+    zuck5.classList.add("visible-zuck5")
+});
+
+// ZUCK 6
+
+const zuck6 = document.getElementById('sixthzuck');
+
+
+zuck6.addEventListener("click", function () {
+    zuck6.classList.add("visible-zuck6")
+});
+
+// ZUCK 7
+
+const zuck7 = document.getElementById('seventhzuck');
+
+
+zuck7.addEventListener("click", function () {
+    zuck7.classList.add("visible-zuck7")
+});
+
+// ZUCK 8
+
+const zuck8 = document.getElementById('eigthzuck');
+
+
+zuck8.addEventListener("click", function () {
+    zuck8.classList.add("visible-zuck8")
+});
+
+// ZUCK 9
+
+const zuck9 = document.getElementById('ninthzuck');
+
+
+zuck9.addEventListener("click", function () {
+    zuck9.classList.add("visible-zuck9")
+});
+
+// ZUCK 10
+
+const zuck10 = document.getElementById('tenthzuck');
+
+
+zuck10.addEventListener("click", function () {
+    zuck10.classList.add("visible-zuck10")
+});
+
+// ZUCK 11
+
+const zuck11 = document.getElementById('elevenzuck');
+
+
+zuck11.addEventListener("click", function () {
+    zuck11.classList.add("visible-zuck11")
+});
+
+// ZUCK 12
+
+const zuck12 = document.getElementById('twelvezuck');
+
+
+zuck12.addEventListener("click", function () {
+    zuck12.classList.add("visible-zuck12")
+});
+
+// ZUCK 13
+
+const zuck13 = document.getElementById('thirteenzuck');
+
+
+zuck13.addEventListener("click", function () {
+    zuck13.classList.add("visible-zuck13")
+});
+
+// ZUCK 14
+
+const zuck14 = document.getElementById('fourteenzuck');
+
+
+zuck14.addEventListener("click", function () {
+    zuck14.classList.add("visible-zuck14")
+});
+
+// ZUCK 15
+
+const zuck15 = document.getElementById('fifthteenzuck');
+
+
+zuck15.addEventListener("click", function () {
+    zuck15.classList.add("visible-zuck15")
+});
+
+
+// ZUCK 16
+
+const zuck16 = document.getElementById('sixteenzuck');
+
+
+zuck16.addEventListener("click", function () {
+    zuck16.classList.add("visible-zuck16")
+});
+
+// ZUCK 17
+
+const zuck17 = document.getElementById('seventeenzuck');
+
+
+zuck17.addEventListener("click", function () {
+    zuck17.classList.add("visible-zuck17")
+});
+
+// ZUCK 18
+
+const zuck18 = document.getElementById('eigthteenzuck');
+
+
+zuck18.addEventListener("click", function () {
+    zuck18.classList.add("visible-zuck18")
+});
+
+// ZUCK 19
+
+const zuck19 = document.getElementById('nineteenzuck');
+
+
+zuck19.addEventListener("click", function () {
+    zuck19.classList.add("visible-zuck19")
+});
+
+// ZUCK 20
+
+const zuck20 = document.getElementById('twentiethzuck');
+
+
+zuck20.addEventListener("click", function () {
+    zuck20.classList.add("visible-zuck20")
+});
